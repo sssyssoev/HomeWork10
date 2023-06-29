628461e3cabce7af79fe1bf883e3b9fd8ad63d2e
ssyssoev <ssyssoev@mail.ru>
2023-06-29 21:00:22 +0300

    Первый коммит к 10-ой домашке

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+
+public class Main {
+    public static void main(String[] args) {
+          task1();
+          task2();
+          task3();
+
+        System.out.println("  ");
+        System.out.println("Printout for HomeWork10 finished");
+
+
+    }
+    public static void task1() {
+        String firstName="Ivanov";
+        String middleName="Ivan";
+        String lastName="Ivanovich";
+        String space=" ";
+        String fullName=firstName+space+middleName+space+lastName;
+        System.out.println("Printout for Task1");
+        System.out.println("  ");
+        System.out.println("ФИО Сотрудника - " + fullName);
+        System.out.println("  ");
+    }
+    public static void task2() {
+        String firstName="Ivanov";
+        String middleName="Ivan";
+        String lastName="Ivanovich";
+        String space=" ";
+        String fullName=firstName+space+middleName+space+lastName;
+        System.out.println("Printout for Task2");
+        System.out.println("  ");
+
+        System.out.println("Данные ФИО Сотрудника - " + fullName.toUpperCase());
+        System.out.println("  ");
+    }
+    public static void task3() {
+        String firstName="Иванов";
+        String middleName="Семён";
+        String lastName="Семёнович";
+        String space=" ";
+        String fullName=firstName+space+middleName+space+lastName;
+        System.out.println("Printout for Task3");
+        System.out.println("  ");
+
+        System.out.print("Данные ФИО Сотрудника - " + fullName.replace('ё','е'));
+        System.out.println("  ");
+    }
+}
