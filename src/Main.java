9156ef3ec8a2edbeec9132977868c7da65a573a3
ssyssoev <ssyssoev@mail.ru>
2023-07-01 12:48:55 +0300

    В домашке отформатировал текст кода

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-          task1();
-          task2();
-          task3();
+        task1();
+        task2();
+        task3();
 
         System.out.println("  ");
         System.out.println("Printout for HomeWork10 finished");
@@ -11,39 +11,42 @@ public class Main {
 
 
     }
+
     public static void task1() {
-        String firstName="Ivanov";
-        String middleName="Ivan";
-        String lastName="Ivanovich";
-        String space=" ";
-        String fullName=firstName+space+middleName+space+lastName;
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String space = " ";
+        String fullName = firstName + space + middleName + space + lastName;
         System.out.println("Printout for Task1");
         System.out.println("  ");
         System.out.println("ФИО Сотрудника - " + fullName);
         System.out.println("  ");
     }
+
     public static void task2() {
-        String firstName="Ivanov";
-        String middleName="Ivan";
-        String lastName="Ivanovich";
-        String space=" ";
-        String fullName=firstName+space+middleName+space+lastName;
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String space = " ";
+        String fullName = firstName + space + middleName + space + lastName;
         System.out.println("Printout for Task2");
         System.out.println("  ");
 
         System.out.println("Данные ФИО Сотрудника - " + fullName.toUpperCase());
         System.out.println("  ");
     }
+
     public static void task3() {
-        String firstName="Иванов";
-        String middleName="Семён";
-        String lastName="Семёнович";
-        String space=" ";
-        String fullName=firstName+space+middleName+space+lastName;
+        String firstName = "Иванов";
+        String middleName = "Семён";
+        String lastName = "Семёнович";
+        String space = " ";
+        String fullName = firstName + space + middleName + space + lastName;
         System.out.println("Printout for Task3");
         System.out.println("  ");
 
-        System.out.print("Данные ФИО Сотрудника - " + fullName.replace('ё','е'));
+        System.out.print("Данные ФИО Сотрудника - " + fullName.replace('ё', 'е'));
         System.out.println("  ");
     }
 }
